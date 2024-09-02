d85447d213b4499a9dad13a376294268a9b38f98
KnMvstr <kenzimakindu@gmail.com>
2024-09-02 11:37:40 +0200

    Import Cleaning

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package repository;
 
 import entity.User;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.Optional;
