e5c8688b127c74855694983d685f8d9dc69e9013
KnMvstr <kenzimakindu@gmail.com>
2024-09-04 10:05:48 +0200

    CarType w/ Panache

--- src/main/java/entity/CarType.java
+++ src/main/java/entity/CarType.java
@@ -3,7 +3,12 @@ package entity;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.github.slugify.Slugify;
+import dto.BrandDTO;
+import dto.CarTypeDTO;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.panache.common.Sort;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +17,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @JsonIdentityInfo(
         generator = ObjectIdGenerators.PropertyGenerator.class,
@@ -22,11 +28,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class CarType extends PanacheEntityBase {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+public class CarType extends PanacheEntity {
 
     private String name;
 
@@ -36,4 +38,65 @@ public class CarType extends PanacheEntityBase {
     @ManyToMany(mappedBy = "carTypes", fetch = FetchType.EAGER)
     @Column(name = "carType")
     private List<Model> models = new ArrayList<>();
+
+    @PrePersist
+    @PreUpdate
+    public void generateSlug() {
+        Slugify slugify = Slugify.builder().build();
+        this.slug = slugify.slugify(this.name);
+    }
+
+    public List<CarTypeDTO> getAllCarTypes() {
+        List<CarType> carTypes = CarType.listAll(Sort.by("name"));
+        return carTypes.stream().map(CarType::toDTO).collect(Collectors.toList());
+    }
+
+    public CarTypeDTO getCarTypeById(Long id) {
+        CarType carType = CarType.findById(id);
+        if (carType == null) {
+            throw new EntityNotFoundException("Could not find carType with id: " + id);
+        }
+        return toDTO(carType);
+    }
+
+    public CarTypeDTO persist(CarTypeDTO carTypeDTO, Long id) {
+        CarType carType;
+        if (id != null) {
+            carType = CarType.findById(id);
+            if (carType == null) {
+                throw new EntityNotFoundException("Could not find carType with id: " + id);
+            }
+        } else {
+            carType = new CarType();
+        }
+        carType.setName(carTypeDTO.getName());
+        CarType.persist(carType);
+        return toDTO(carType);
+    }
+
+    public void delete(Long id) {
+        CarType carType = CarType.findById(id);
+        if (carType == null) {
+            throw new EntityNotFoundException("Could not find carType with id: " + id);
+        }
+        CarType.deleteById(carType);
+    }
+
+    public static CarType fromDTO(CarTypeDTO dto) {
+        CarType carType = new CarType();
+        carType.name = dto.getName();
+        return carType;
+    }
+
+    public void updateFromDTO(CarTypeDTO dto) {
+        this.name = dto.getName();
+    }
+
+    public static CarTypeDTO toDTO(CarType carType) {
+        CarTypeDTO dto = new CarTypeDTO();
+        dto.setId(carType.id);
+        dto.setName(carType.name);
+        return dto;
+    }
+
 }
