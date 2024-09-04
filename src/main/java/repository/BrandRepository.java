6cc4ddbb42fd1f09b32caa1602c6b544f7a1bda5
KnMvstr <kenzimakindu@gmail.com>
2024-09-04 09:44:49 +0200

    Unnecessary class
    Due to PanacheEntity abstract class in the entity the Repository and Service layer are not necessary for most common api & db transaction

--- src/main/java/repository/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.Brand;
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class BrandRepository implements PanacheRepositoryBase<Brand, Long> {
-}
