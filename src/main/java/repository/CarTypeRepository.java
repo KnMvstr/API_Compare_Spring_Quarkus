3aa076381bc8f8d7b63a5d84f4e09cee1f606377
KnMvstr <kenzimakindu@gmail.com>
2024-09-04 10:07:32 +0200

    Delete CarType Repository

--- src/main/java/repository/CarTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import entity.CarType;
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class CarTypeRepository implements PanacheRepositoryBase<CarType, Long> {
-}
