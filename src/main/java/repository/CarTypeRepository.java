856f445a3eb706b130eb6510d66092901d0965e9
KnMvstr <kenzimakindu@gmail.com>
2024-09-11 14:17:51 +0200

    Updated repository
    In order to display the list of colors and cartypes in ModelDTO

--- src/main/java/repository/CarTypeRepository.java
+++ src/main/java/repository/CarTypeRepository.java
@@ -1,9 +1,22 @@
 package repository;
 
 import entity.CarType;
+import entity.Color;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Collections;
+import java.util.List;
+
 @ApplicationScoped
 public class CarTypeRepository implements PanacheRepositoryBase<CarType, Long> {
+
+    // Method to find multiple cartypes based on a list of IDs
+    // Useful to pot multiple cartypes on a new model
+    public List<CarType> findByIds(List<Long> ids) {
+        if (ids == null || ids.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return list("id in ?1", ids);
+    }
 }
--- src/main/java/repository/ColorRepository.java
+++ src/main/java/repository/ColorRepository.java
@@ -1,9 +1,22 @@
 package repository;
 
 import entity.Color;
+import entity.Model;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Collections;
+import java.util.List;
+
 @ApplicationScoped
 public class ColorRepository implements PanacheRepositoryBase<Color, Long> {
+
+    // Method to find multiple colors based on a list of IDs
+    // Useful to pot multiple color on a new model
+    public List<Color> findByIds(List<Long> ids) {
+        if (ids == null || ids.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return list("id in ?1", ids);
+    }
 }
